4889611f801ce67860b027feb9a7aeba3324c833
robertej19 <robertej@mit.edu>
2021-02-05 08:14:48 -0500

    dvep cutter cleaned up

--- src/1_hipo_root_cuts/dvep_cutter/root_batch_dvpip_cutter.py
+++ src/1_hipo_root_cuts/dvep_cutter/root_batch_dvpip_cutter.py
@@ -4,26 +4,38 @@ import time
 import shutil
 from shutil import copyfile
 
-#data_dir = "/mnt/d/CLAS12Data/converted_skim8_filters/"
-data_dir = "../root_skims/"
-output_dir = "DVEP_roots/"
-data_list = os.listdir(data_dir)
+#This project
+from utils import data_getter
+from utils import query_maker
+from utils import file_maker
+from plot_makers import plot_maker_hist_plotter
 
+
+
+fs = data_getter.get_json_fs()
+
+datafile_dir = "F18_Inbending_SangbaekSkim_20200129/"
+data_dir = fs['base_dir']+fs['data_dir']+fs["data_2_dir"]+datafile_dir
+data_list = os.listdir(data_dir)
 print(data_list)
 
 
-root_macro = "scriptPi0_new.C"
+#For FD
+root_macro_script = "dvpip_cutterFD.C"
+data_out_dir = "F18_Inbending_FD_SangbaekSkim_0_20210205/"
+#For CD
+#root_macro_script = "dvpip_cutterCD.C"
+#data_out_dir = "F18_Inbending_CD_SangbaekSkim_0_20210205/"
+
+root_macro = fs['base_dir']+fs['src_dir']+fs['dvep_cut_dir']+root_macro_script
+
+output_dir = fs['base_dir']+fs['data_dir']+fs["data_3_dir"]+data_out_dir
+file_maker.make_dir(output_dir)
+
+
 
 default_root_outname = "output_root_file.root"
 default_root_inname = "input_root_file.root"
-
-infile = "testerfile.txt"
-outfile = data_dir+"fixed.txt"
-
-
-#new_list = data_list[1:3]
-#print(new_list)
-
 for count,file in enumerate(data_list):
     print("Trying to process file {}".format(count))
     copyfile(data_dir+file,default_root_inname)
